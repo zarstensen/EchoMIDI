f306c12caf4a1d5d4178371c894f41ebece9f0fd
karstensensensen <61250140+karstensensensen@users.noreply.github.com>
2022-06-29 22:27:43 +0200

    added EchoManager class

---
+++ EchoMIDIApp/include/EchoManager.h
@@ -0,0 +1,232 @@
+#pragma once
+
+#include <Echoer.h>
+
+/// @brief class reseponsible for handling a system of midi input devices and their corresponding target output devices.
+/// stores properties for midi devices that are currently being used / has been used, unless forgetMidi(In/Out)Device() is excplicitly called.
+class EchoManager
+{
+public:
+
+	struct MidiOutProps
+	{
+		bool avaliable;
+		std::map<std::string, bool> mute;
+		std::map<std::string, std::string> focus_mute;
+	};
+
+	struct MidiInProps
+	{
+		/// @brief wether the midi device is currently plugged in to the system.
+		bool avaliable;
+		/// @brief wether the midi device should currently be sending data to its targets.
+		bool echo;
+		EchoMIDI::Echoer echoer;
+
+	};
+
+	/// @brief updates the avaliability of all the currently stored midi devices, and adds any new midi devices connected to the computer.
+	void syncMidiDevices()
+	{
+		// loop over all the connected midi inputs and remove / add any missing midi devices.
+
+		std::map<std::string, bool> avaliable_devices;
+		std::vector<std::string> new_devices;
+
+		for (auto& [name, _] : m_midi_inputs)
+			avaliable_devices[name] = false;
+
+		for (UINT id = 0; id < midiInGetNumDevs(); id++)
+		{
+			std::string input_name = EchoMIDI::getMidiInputName(id);
+			
+			if (avaliable_devices.contains(input_name))
+			{
+				avaliable_devices[input_name] = true;
+
+				// if device has become avaliable, check if it should be sending.
+
+				MidiInProps& midi_input = m_midi_inputs[input_name];
+
+				if (!midi_input.avaliable && midi_input.echo)
+				{
+					midi_input.echoer.open(EchoMIDI::getMidiInIDByName(input_name));
+					midi_input.echoer.start();
+					new_devices.push_back(input_name);
+				}
+			}
+			else
+			{
+				// a new device was discovered.
+				m_midi_inputs[input_name] = MidiInProps{true, false };
+				new_devices.push_back(input_name);
+			}
+		}
+
+		// update the avaliability of all the devices.
+		for (auto& [name, props] : m_midi_inputs)
+		{
+			if (avaliable_devices.contains(name))
+			{
+
+				props.avaliable = avaliable_devices[name];
+
+				// device is no longer avaliable, stop echoing.
+
+				if (!props.avaliable)
+				{
+					if(props.echoer.isEchoing())
+						props.echoer.stop();
+
+					if (props.echoer.isOpen())
+						props.echoer.close();
+				}
+			}
+		}
+
+		// do the same for the midi outputs
+
+		avaliable_devices.clear();
+
+		for (auto& [name, _] : m_midi_outputs)
+			avaliable_devices[name] = false;
+
+		for (UINT id = 0; id < midiOutGetNumDevs(); id++)
+		{
+			std::string output_name = EchoMIDI::getMidiOutputName(id);
+
+			if (avaliable_devices.contains(output_name))
+			{
+				avaliable_devices[output_name] = true;
+
+				// if device has become avaliable, make sure all the Echoers have it as a target.
+
+				MidiOutProps& midi_out_props = m_midi_outputs[output_name];
+
+				if (!midi_out_props.avaliable)
+				{
+					for (auto& [input_name, _] : m_midi_inputs)
+					{
+						setTargetMute(output_name, input_name, midi_out_props.mute[input_name]);
+						setTargetFocusMute(output_name, input_name, midi_out_props.focus_mute[input_name]);
+					}
+				}
+			}
+			else
+			{
+				// a new device was discovered.
+				m_midi_outputs[output_name] = MidiOutProps{ true };
+			}
+		}
+
+		for (auto& [name, props] : m_midi_outputs)
+		{
+			if (avaliable_devices.contains(name))
+			{
+				props.avaliable = avaliable_devices[name];
+
+				// device is no longer avaliable, remove it as a target from all the active echoers.
+
+				if (!props.avaliable)
+				{
+					UINT device_id = EchoMIDI::getMidiOutIDByName(name);
+					for (auto& [input_name, input_prop] : m_midi_inputs)
+						if (input_prop.avaliable && input_prop.echoer.getTargets().contains(device_id))
+							input_prop.echoer.remove(device_id);
+				}
+			}
+		}
+
+		// make sure all newly avaliable input devices have the corrent targets
+		for (const std::string& new_input : new_devices)
+			for (auto& [out_name, _] : m_midi_outputs)
+				tryAddTarget(out_name, new_input);
+	}
+
+	/// @brief set the send state for a input device
+	/// 
+	/// if the send state is true, the input device will be opened and start echoing to its output targets.
+	/// if set to true whilst the midi device is unavaliable, the Echoer will be opened as soon as the device is avaliable again.
+	/// 
+	/// @param name the name of the midi device
+	/// @param val the send state
+	void setInEcho(const std::string& name, bool val)
+	{
+		if(m_midi_inputs[name].echo != val && m_midi_inputs[name].avaliable)
+			if (val)
+			{
+				m_midi_inputs[name].echoer.open(EchoMIDI::getMidiInIDByName(name));
+				m_midi_inputs[name].echoer.start();
+			}
+			else
+			{
+				m_midi_inputs[name].echoer.stop();
+				m_midi_inputs[name].echoer.close();
+			}
+
+		m_midi_inputs[name].echo = val;
+	}
+
+	/// @brief set the mute state of the target output, for the sources Echoer instance.
+	void setTargetMute(const std::string& target, const std::string& source, bool val)
+	{
+		m_midi_outputs[target].mute[source] = val;
+		
+		tryAddTarget(target, source);
+
+		UINT out_id = EchoMIDI::getMidiOutIDByName(target);
+
+		if(m_midi_inputs[source].avaliable && m_midi_inputs[source].echo && m_midi_inputs[source].echoer.getTargets().contains(out_id))
+			m_midi_inputs[source].echoer.setMute(out_id, val);
+	}
+
+	void setTargetFocusMute(const std::string& target, const std::string& source, const std::string& val)
+	{
+		m_midi_outputs[target].focus_mute[source] = val;
+		
+		tryAddTarget(target, source);
+
+		UINT out_id = EchoMIDI::getMidiOutIDByName(target);
+
+		if (m_midi_inputs[source].avaliable && m_midi_inputs[source].echo && m_midi_inputs[source].echoer.getTargets().contains(out_id))
+			m_midi_inputs[source].echoer.focusMute(out_id, val);
+
+	}
+
+	bool inIsAvaliable(const std::string& name)
+	{
+		return m_midi_inputs[name].avaliable;
+	}
+
+	bool outIsAvaliable(const std::string& name)
+	{
+		return m_midi_outputs[name].avaliable;
+	}
+
+	const std::map<std::string, MidiInProps>& getMidiInputs() { return m_midi_inputs; }
+	
+	const std::map<std::string, MidiOutProps>& getMidiOutputs() { return m_midi_outputs; }
+
+
+private:
+
+	// initializes the source Echoer with the target outptus devices properties, if the target is not muted for the source.
+	void tryAddTarget(const std::string& target, const std::string& source)
+	{
+		MidiInProps& in_props = m_midi_inputs[source];
+		MidiOutProps& out_props = m_midi_outputs[target];
+
+		UINT target_id = EchoMIDI::getMidiOutIDByName(target);
+
+		if (in_props.avaliable && out_props.mute.contains(source) && !out_props.mute[source] && !in_props.echoer.getTargets().contains(target_id))
+		{
+			m_midi_inputs[source].echoer.add(target_id);
+			m_midi_inputs[source].echoer.focusMute(target_id, m_midi_outputs[target].focus_mute[source]);
+			m_midi_inputs[source].echoer.setMute(target_id, false);
+		}
+	}
+
+	std::map<std::string, MidiInProps> m_midi_inputs;
+	std::map<std::string, MidiOutProps> m_midi_outputs;
+};
+
